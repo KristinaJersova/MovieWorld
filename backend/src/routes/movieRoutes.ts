ebfdbab8595694ab2ab8d4e8fe5d7dc2ea99236a
Kristina Jeršova <kristinajershova5@gmail.com>
2026-05-13 15:23:01 +0300

    change movie routes, change put, post, delete ...

--- backend/src/routes/movieRoutes.ts
+++ backend/src/routes/movieRoutes.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import {getMovies, getMovieById, createMovie, updateMovie, deleteMovie,} from "../controllers/movieController.js";
+import {getMovies,getMovieById,createMovie,updateMovie,deleteMovie,} from "../controllers/movieController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import { roleMiddleware } from "../middleware/roleMiddleware.js";
+import { validate } from "../middleware/validate.js";
+import { movieSchema } from "../validators/movieValidator.js";
 
 const router = Router();
 
 router.get("/", getMovies);
 router.get("/:id", getMovieById);
 
-router.post("/", authMiddleware, roleMiddleware(["admin"]), createMovie);
-router.put("/:id", authMiddleware, roleMiddleware(["admin"]), updateMovie);
-router.delete("/:id", authMiddleware, roleMiddleware(["admin"]), deleteMovie);
+router.post("/",authMiddleware,roleMiddleware(["admin"]),validate(movieSchema),createMovie);
+router.put("/:id",authMiddleware,roleMiddleware(["admin"]),validate(movieSchema),updateMovie);
+router.delete("/:id",authMiddleware,roleMiddleware(["admin"]),deleteMovie);
 
 export default router;
