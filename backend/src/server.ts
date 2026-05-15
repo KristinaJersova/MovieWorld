2fef213ef23f711843451fc9b8a12cf12d4f6055
Kristina Jeršova <kristinajershova5@gmail.com>
2026-05-15 16:42:51 +0300

    added swager import, use

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import movieRoutes from "../src/routes/movieRoutes.js";
-import directorRoutes from "../src/routes/direktorRoutes.js";
+
+import movieRoutes from "./routes/movieRoutes.js";
+import directorRoutes from "./routes/direktorRoutes.js";
 import actorRoutes from "./routes/actorRoutes.js";
 import genreRoutes from "./routes/genreRoutes.js";
 import movieRelationRoutes from "./routes/movieRelationRoutes.js";
@@ -10,12 +11,17 @@ import ratingRoutes from "./routes/ratingRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import omdbRoutes from "./routes/omdbRoutes.js";
 
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./swagger.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
@@ -30,7 +36,6 @@ app.use("/api/genres", genreRoutes);
 app.use("/api/movies", movieRelationRoutes);
 app.use("/api/ratings", ratingRoutes);
 app.use("/api/auth", authRoutes);
-
 app.use("/api/omdb", omdbRoutes);
 
 const PORT = process.env.PORT || 3000;
